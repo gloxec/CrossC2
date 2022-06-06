442ade106dde4359301d9c24ba0efc10d0290619
gloxec <gloxec@gmail.com>
2022-06-06 20:25:34 +0800

    update release
    Former-commit-id: 3ea37ff7770f6912ba9b54d0ca235e470f574132

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /tmp/
+for module in `find ./third-party | grep "makefile" | awk -F'/makefile' '{print $1}'`
+do
+    echo $module
+    rm -rf $module
+done
+zip -r third-party.zip third
